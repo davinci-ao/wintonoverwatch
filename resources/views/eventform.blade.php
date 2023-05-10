2d1e5fc3277f15e69efcdad04ef1bc7e01453ee3
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-10 16:52:27 +0200

    Fix eventforms (accidentally removed styles)

--- resources/views/eventform.blade.php
+++ resources/views/eventform.blade.php
@@ -11,31 +11,35 @@
     <div class="py-7">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <form method="POST" action="/event/create">
+                <form method="POST" action ="/event/create">
                     @csrf
-                    Name: <input type="text" name="name" required><br>
-                    Description: <input type="text" name="description" required><br>
-                    StartDate: <input type="datetime-local" value="" id="startDate" name="startDate" min="{{Carbon\Carbon::now()->format("Y-m-d")}}T{{Carbon\Carbon::now()->format("H:i")}}" required><br>
-                    EndDate: <input type="datetime-local" value="" id="endDate" name="endDate" required><br>
-                    <button type="submit" style="padding: 5px; border-width: 2px;">Submit</button>
+                    <div class="w-4/5 m-5">
+                        <p class="inline-block w-2/12 text-center font-bold m-5 text-2xl">Name:</p>
+                        <input type="text" name="name" placeholder="Enter name here..." class="bg-transparent mx-auto border-b-2 w-9/12 h-12 text-2xl outline-none"><br>
 
-                    <script>
-                        let txtStartDate = document.getElementById("startDate");
-                        let txtEndDate = document.getElementById("endDate");
+                        <p class="inline-block w-2/12 text-center font-bold m-5 text-2xl align-top">Description:</p>
+                        <textarea rows="4" cols="50" name="description" placeholder="Enter description here..."  class="bg-transparent mx-auto border-b-2 w-9/12 h-w-3.5 text-2xl outline-none"></textarea><br>
+                        StartDate: <input type="datetime-local" value="" id="startDate" name="startDate" min="{{Carbon\Carbon::now()->format("Y-m-d")}}T{{Carbon\Carbon::now()->format("H:i")}}" required><br>
+                        EndDate: <input type="datetime-local" value="" id="endDate" name="endDate" required><br>
+                        <button type="submit" style="padding: 5px; border-width: 2px;">Submit</button>
 
-                        if (txtStartDate) {
-                            txtStartDate.addEventListener("change", (event) => {
-                                console.log(txtStartDate.value);
-                                let date = new Date(txtStartDate.value);
-                                console.log(date.toString())
-                                date.setDate(date.getDate() + 3);
-                                console.log(date.toString())
+                        <script>
+                            let txtStartDate = document.getElementById("startDate");
+                            let txtEndDate = document.getElementById("endDate");
 
-                                txtEndDate.min = date.toISOString().split(".")[0];
-                                console.log(txtEndDate.min);
-                            });
-                        }
-                    </script>
+                            if (txtStartDate) {
+                                txtStartDate.addEventListener("change", (event) => {
+                                    console.log(txtStartDate.value);
+                                    let date = new Date(txtStartDate.value);
+                                    console.log(date.toString())
+                                    date.setDate(date.getDate() + 3);
+                                    console.log(date.toString())
+
+                                    txtEndDate.min = date.toISOString().split(".")[0];
+                                    console.log(txtEndDate.min);
+                                });
+                            }
+                        </script>
                 </form>
             </div>
         </div>
