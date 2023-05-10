e0ca8402f5cd68a61ea0b34ce96c659e30383d79
99009744 <55435447+99009744@users.noreply.github.com>
2023-05-10 10:44:08 +0200

    Edited layout of the create event page
    - Layout create event page

--- resources/views/eventform.blade.php
+++ resources/views/eventform.blade.php
@@ -9,13 +9,19 @@
     </x-slot>
 
     <div class="py-7">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 w-4/5 m-auto">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg w-full m-auto">
                 <form method="POST" action ="/event/create">
                     @csrf
-                    Name: <input type="text" name="name"><br>
-                    Description: <input type="text" name="description"><br>
-                    <button type="submit" style="padding: 5px; border-width: 2px;">Submit</button>
+                    <div class="w-4/5 m-5">
+                        <p class="inline-block w-2/12 text-center font-bold m-5 text-2xl">Name:</p> 
+                        <input type="text" name="name" placeholder="Enter name here..." class="bg-transparent mx-auto border-b-2 w-9/12 h-12 text-2xl outline-none"><br>
+
+                        <p class="inline-block w-2/12 text-center font-bold m-5 text-2xl align-top">Description:</p> 
+                        <textarea rows="4" cols="50" name="description"placeholder="Enter description here..."  class="bg-transparent mx-auto border-b-2 w-9/12 h-w-3.5 text-2xl outline-none"></textarea><br>
+                    </div>
+                   
+                    <button type="submit" class="uppercase bg-blue-500 text-gray-100 text-lg w-1/5 m-10 font-extrabold py-4 px-8 rounded-3xl hover:bg-sky-700">Submit</button>
                 </form>
             </div>
         </div>
