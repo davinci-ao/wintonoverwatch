8b5b2043eae4dfeb6e8f4d2e117388066ec3717e
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-10 16:16:19 +0200

    Add script to filter dates

--- resources/views/eventform.blade.php
+++ resources/views/eventform.blade.php
@@ -13,11 +13,29 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 <form method="POST" action="/event/create">
                     @csrf
-                    Name: <input type="text" name="name"><br>
-                    Description: <input type="text" name="description"><br>
-                    StartDate: <input type="datetime-local" name="startDate"><br>
-                    EndDate: <input type="datetime-local" name="endDate"><br>
-                    <button type="submit" style="padding: 5px; border-width: 2px;">Submit</button>
+                    Name: <input type="text" name="name" required><br>
+                    Description: <input type="text" name="description" required><br>
+                    StartDate: <input type="datetime-local" value="" id="startDate" name="startDate" min="{{Carbon\Carbon::now()->format("Y-m-d")}}T{{Carbon\Carbon::now()->format("H:i")}}" required><br>
+                    EndDate: <input type="datetime-local" value="" id="endDate" name="endDate" required><br>
+=                    <button type="submit" style="padding: 5px; border-width: 2px;">Submit</button>
+
+                    <script>
+                        let txtStartDate = document.getElementById("startDate");
+                        let txtEndDate = document.getElementById("endDate");
+
+                        if (txtStartDate) {
+                            txtStartDate.addEventListener("change", (event) => {
+                                console.log(txtStartDate.value);
+                                let date = new Date(txtStartDate.value);
+                                console.log(date.toString())
+                                date.setDate(date.getDate() + 3);
+                                console.log(date.toString())
+
+                                txtEndDate.min = date.toISOString().split(".")[0];
+                                console.log(txtEndDate.min);
+                            });
+                        }
+                    </script>
                 </form>
             </div>
         </div>
