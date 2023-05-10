e0ce21beb6518319eacfc00dc042f5b174723a3e
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-10 11:35:05 +0200

    add startDate, endDate to events table

--- database/migrations/2023_04_19_083517_create_events_table.php
+++ database/migrations/2023_04_19_083517_create_events_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
+            $table->dateTime('startDate')->nullable();
+            $table->dateTime('endDate')->nullable();
             $table->timestamps();
         });
     }
