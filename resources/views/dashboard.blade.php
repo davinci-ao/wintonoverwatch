4f4c632460cf5f6af109db1bf368da17c1c501b5
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-12 11:32:21 +0200

    Fix wrong starting tag

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,11 +19,11 @@
                     </h1>
 
                     <h1 class="text-xl text-gray-900 dark:text-white font-extrabold w-1/12 inline-block">Date: </h1>
-                    <h1 class="mt-8 text-xl font-medium text-gray-900 dark:text-white w-3/12 inline-block">{{$data->startDate}} - {{$data->endDate}}</h2>
+                    <h1 class="mt-8 text-xl font-medium text-gray-900 dark:text-white w-6/12 inline-block">{{$data->startDate}} -> {{$data->endDate}}</h1>
                     <p class="mt-6 mb-3 text-gray-500 dark:text-gray-400 leading-relaxed">
                     {{$data->description}}
                     </p>
-                    
+
                     <a href="/event/{{$data->id}}" class="border border-gray-500 bg-blue-500 text-m text-white py-2 px-2 rounded-3xl transition hover:bg-sky-700">Show event</a>
 
                 </div>
