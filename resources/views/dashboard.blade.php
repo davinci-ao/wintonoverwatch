e92d7bce1fbc6a93ae1a219abad283d7098ad293
99009744 <55435447+99009744@users.noreply.github.com>
2023-05-15 18:57:46 +0200

    Updated dashboard
    Made the full div clickable to go to the event.
    Made the button to edit the event under the div, only people with the role 1 see this div.
    Fixed evenement aanmaken button only visible for people with role 1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,15 +3,18 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight float-left mt-4">
             {{ __('Evenementen') }}
         </h2>
-        <a href="/eventform" class="uppercase bg-blue-500 text-gray-100 text-lg w-fit font-extrabold py-3 px-6 rounded-3xl float-right hover:bg-sky-700">
-            {{ __('Aanmaken Evenementen') }}
-        </a>
+        @if(auth()->user()->role_id == 1)
+            <a href="/eventform" class="uppercase bg-blue-500 text-gray-100 text-lg w-fit font-extrabold py-3 px-6 rounded-3xl float-right hover:bg-sky-700">
+                {{ __('Aanmaken Evenementen') }}
+            </a>
+        @endif
     </x-slot>
 
     <div class="py-7">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 @foreach ($events as $key => $data)
+                <a href="/event/{{$data->id}}" class="block">
                 <div class="lg:p-6 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700 lg:flex">
                     <div class="w-3/12 inline-block mr-5 justify-items-start float-left">
                         <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" alt="">
@@ -27,14 +30,18 @@
                         <p class="mt-2 mb-3 text-gray-500 dark:text-gray-400 leading-relaxed">
                         {{ Str::limit($data->description, 400) }}
                         </p>
-                        <a href="/event/{{$data->id}}" class="border border-gray-500 bg-blue-500 text-m text-white py-2 px-2 rounded-3xl transition hover:bg-sky-700">Show event</a>
-                        @if(auth()->user()->role_id == 1)
-                            <a href="{{ route('eventEdit', $data->id) }}" class="border border-gray-500 bg-green-500 text-m text-white basis-full py-2 px-10 rounded-3xl transition hover:bg-green-700"> Edit </a>
-                        @endif
+                        
                     </div>
                 </div>
+                </a>
+                    @if(auth()->user()->role_id == 1)
+                        <div class="bg-gray-100 px-4 py-1 flex justify-end">
+                            <a href="{{ route('eventEdit', $data->id) }}" class="text-gray-700 hover:text-gray-900"> Edit </a>
+                            </div>
+                    @endif
+                                
                 @endforeach
-
+    
             </div>
         </div>
     </div>
