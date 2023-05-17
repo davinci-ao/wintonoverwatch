a165c2da25b77aa2e52f64752958b4dcfcec2cf2
DaanM24 <99054517@mydavinci.nl>
2023-05-17 14:18:11 +0200

    Updated layout slightly

--- resources/views/eventcompanies.blade.php
+++ resources/views/eventcompanies.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight float-left mt-4">
-            {{ __('Company') }}
+            {{ __('Bedrijven:') }}
         </h2>
     </x-slot>
 
@@ -10,12 +10,10 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 <form method="POST" action ="/event/company/add">
                     @csrf
-                    <div class="w-4/5 m-5">
-                        
-                        <p class="inline-block w-2/12 text-center font-bold m-5 text-2xl">Bedrijven:</p><br> 
+                    <div class="w-4/5 m-5 ml-10">
                         @foreach($companies as $key => $data)
-                        <input type="checkbox" id="{{$data->name}}" name="{{$key}}" value="{{$data->id}}">
-                        <label for="{{$data->name}}">{{$data->name}}</label><br>
+                        <input type="checkbox" id="{{$data->name}}" name="{{$key}}" value="{{$data->id}}" class="mb-3 mt-2">
+                        <label for="{{$data->name}}" class="text-center font-bold m-5 ml-2 text-2xl align-top" >{{$data->name}}</label><br>
                         @endforeach
 
                         <button type="submit" class="uppercase bg-blue-500 text-gray-100 text-lg w-1/5 m-10 font-extrabold py-4 px-8 rounded-3xl hover:bg-sky-700">Submit</button>
