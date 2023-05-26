52b5cad0061a193ed786bdede6410cfc2a9e867c
99009744 <55435447+99009744@users.noreply.github.com>
2023-05-26 10:10:39 +0200

    Updated dashboard
    Fixed description was nog limited anymore.
    Added Image static size

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,7 +18,7 @@
                         <a href="/event/{{$data->id}}" class="block">
                         <div class="lg:p-6 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700 lg:flex hover:transform hover:border-gray-600">
                             <div class="w-3/12 inline-block mr-5 justify-items-start float-left">
-                                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" alt="">
+                                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" alt="" class="w-72 h-64">
                             </div>    
                             <div class="w-8/12 inline-block">
                                 <h1 class="text-5xl font-bold text-gray-900 dark:text-white font-extrabold">
@@ -28,7 +28,7 @@
                                 <h1 class="text-xl text-gray-900 dark:text-white font-extrabold w-full inline-block">Date: {{\Carbon\Carbon::parse($data->startDate)->format('H:i d/m/Y')}} -> {{\Carbon\Carbon::parse($data->endDate)->format('H:i d/m/Y')}}</h1>
                                 <h1 class="text-xl text-gray-900 dark:text-white font-extrabold w-full inline-block">Location: {{$data->location}}</h1>
                                 <p class="mt-2 mb-3 text-gray-500 dark:text-gray-400 leading-relaxed">
-                                {{$data->description}}
+                                {{ Str::limit($data->description, 400) }}
                                 </p>
                                 
                             </div>
